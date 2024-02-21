4d1c8fcf5eff252e1338f38aaad3e7cdd7fe4834
Roni Juppi <roju269@gmail.com>
2024-02-21 15:27:08 +0200

    Test revalidate 60 for layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import { Metadata } from "next";
 
 import "~/styles/globals.css";
 
+export const revalidate = 60;
+
 export const metadata: Metadata = {
   title: "HN-Next",
   description: "Hacker News client with Next.js/React",
