d35d2aa2fb45e5975babbb4b60efacc278957b6c
Roni Juppi <roju269@gmail.com>
2024-02-21 15:33:09 +0200

    Revert "Test revalidate 60 for layout"
    This reverts commit 4d1c8fcf5eff252e1338f38aaad3e7cdd7fe4834.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,8 +3,6 @@ import { Metadata } from "next";
 
 import "~/styles/globals.css";
 
-export const revalidate = 60;
-
 export const metadata: Metadata = {
   title: "HN-Next",
   description: "Hacker News client with Next.js/React",
